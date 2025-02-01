a76444fbd2afa4d7cbd3af248b6b1172c424f4f8
Dnotme03 <d.not.me03@gmail.com>
2025-02-01 22:19:53 +0530

    Update image_urls.py

--- data/image_urls.py
+++ data/image_urls.py
@@ -1,3 +1,17 @@
 izumi = [
-    "https://combative-sarine-eypz-god-d4cce0fc.koyeb.app/image"
+    "https://files.catbox.moe/lkf5z7.jpg",
+"https://files.catbox.moe/5fee1q.jpg",
+"https://files.catbox.moe/4do22t.jpg",
+"https://files.catbox.moe/74i9km.jpg",
+"https://files.catbox.moe/66injc.jpg",
+"https://files.catbox.moe/d4ybtb.jpg",
+"https://files.catbox.moe/3bwv4r.jpg",
+"https://files.catbox.moe/uhl6qt.jpg",
+"https://files.catbox.moe/zjsqoa.jpg",
+"https://files.catbox.moe/czlwfq.jpg",
+"https://files.catbox.moe/1jtfmn.jpg",
+"https://files.catbox.moe/3a9g5v.jpg",
+"https://files.catbox.moe/a6wzip.jpg",
+"https://files.catbox.moe/dq9seg.jpg",
+"https://files.catbox.moe/fek4ae.jpg"
 ]
